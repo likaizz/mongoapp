b1d511f4040c349e9ee22c46c0cd9049397ead86
likaizz <654511905@qq.com>
2018-06-20 00:14:36 +0800

    yml配置map

--- src/main/java/com/example/mongoapp/MongoappApplication.java
+++ src/main/java/com/example/mongoapp/MongoappApplication.java
@@ -3,6 +3,7 @@ package com.example.mongoapp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -15,11 +16,15 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 //@Configuration
-@SpringBootApplication
 //@PropertySource({"classpath:application-app0.properties","classpath:application.properties"})
+@SpringBootApplication
 @RestController
+@ConfigurationProperties
 @EnableAsync
 public class MongoappApplication {
     @Value("${lol}")
@@ -38,19 +43,33 @@ public class MongoappApplication {
         return sdf;
     }
 
+    private Map<String, Object> myMap = new HashMap<>();
+
+    @Bean("myMap")
+    public Map getMyMap() {
+        return myMap;
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MongoappApplication.class, args);
         String[] beans = context.getBeanDefinitionNames();
         Arrays.sort(beans);
         for (String x : beans) {
-			System.out.println(x);
+            System.out.println(x);
+        }
+        Map map = context.getBean("myMap", Map.class);
+        System.out.println(map);
+        Set<String> set = map.keySet();
+        for (String x : set) {
+            Object val = map.get(x);
+            System.out.println(x + ":" + val + "  ,class:" + val.getClass());
         }
     }
 
     @GetMapping("/otherPort")
     public Integer getOtherPort(int port) {
         RestTemplate req = new RestTemplate();
-        ResponseEntity<Integer> resp=req.exchange("http://localhost:"+port+"/myPort", HttpMethod.GET,null,Integer.class);
+        ResponseEntity<Integer> resp = req.exchange("http://localhost:" + port + "/myPort", HttpMethod.GET, null, Integer.class);
         return resp.getBody();
     }
 
